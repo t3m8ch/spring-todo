7997954fba822ff1bb0d74fa1f48bf832c3bdb48
Kudyakov Artem <t3m8ch@yandex.ru>
2022-07-06 22:40:12 +0400

    docs: Add swagger

--- src/main/kotlin/io/github/t3m8ch/springtodo/AppConfiguration.kt
+++ src/main/kotlin/io/github/t3m8ch/springtodo/AppConfiguration.kt
@@ -2,13 +2,36 @@ package io.github.t3m8ch.springtodo
 
 import io.github.t3m8ch.springtodo.db.dao.interfaces.TodoDAO
 import io.github.t3m8ch.springtodo.db.dao.memory.MemoryTodoDAO
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springdoc.core.GroupedOpenApi
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.List
+
 
 @Configuration
 class AppConfiguration {
     @Bean
     fun todoDAO(): TodoDAO {
         return MemoryTodoDAO()
+    }
+
+    @Bean
+    fun publicUserApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("Todos")
+            .pathsToMatch("/todos/**")
+            .build()
+    }
+
+    @Bean
+    fun customOpenApi(): OpenAPI {
+        return OpenAPI().info(
+            Info().title("Application API")
+                .version("0.0.1")
+                .description("Something")
+        )
     }
 }
